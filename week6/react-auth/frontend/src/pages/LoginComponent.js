535d31f41c9e353d066a9c13bb68f769bccaf6fc
Runzhou Zhu <runzhouz@metropolia.fi>
2024-09-23 10:11:29 +0300

    session storage

--- week6/react-auth/frontend/src/pages/LoginComponent.js
+++ week6/react-auth/frontend/src/pages/LoginComponent.js
@@ -18,7 +18,7 @@ const LoginComponent = ({ setIsAuthenticated }) => {
 
       if (response.ok) {
         const user = await response.json();
-        localStorage.setItem("user", JSON.stringify(user));
+        sessionStorage.setItem("user", JSON.stringify(user));
         console.log("User log in successfully!");
         setIsAuthenticated(true);
         navigate("/");
